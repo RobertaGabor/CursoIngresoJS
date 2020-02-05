203ebeecf14f54e2c047f32a3e62838ca7194fa0
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-05 20:24:29 -0300

    E/S Ej 1 sabado
    consultar si esta bien

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,18 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho=prompt("Ingresar ancho de rectangulo","Ej:20");
+	ancho=parseInt(ancho);
+	largo=prompt("Ingresar largo de rectangulo","Ej:10");
+	largo=parseInt(largo);
+
+	perimetro=ancho*2+largo*2;
+
+	alert(perimetro);
+	
 
 }
