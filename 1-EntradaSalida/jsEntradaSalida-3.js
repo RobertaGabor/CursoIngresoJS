e2e4c65cf76d2a7e85479de8d6153bd6275f4868
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-05 10:35:51 -0300

    Ejercicio 3 hecho
    Búsqueda de ID

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	var nombre;
+	nombre=document.getElementById('elNombre').value;//g con tab y shift id. buscar id en html y poner el value para que no salte error en alert
+	alert(nombre);
+
 	
 }
 
