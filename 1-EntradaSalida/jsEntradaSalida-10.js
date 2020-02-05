34714e540ee656de93842983b1ea932e80302a48
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-05 19:54:51 -0300

    Ejercicio 10 hecho
    prompts con precio y descuento, revisar casilla sobrante

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,18 +4,30 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	//var importe;
+	//var importeConDescuento;
+
+	//importe=document.getElementById('importe').value;
+	//importe=parseInt(importe);
+
+	//importeConDescuento=importe+importe*25/100;
+
+	//document.getElementById('resultado').value=importeConDescuento;
+
 	var importe;
 	var descuento;
 	var importeConDescuento;
 
-	importe=document.getElementById('importe').value;
+	importe=prompt("Ingrese el importe");//que hacer con la casilla de importe
 	importe=parseInt(importe);
-	descuento=importe*25/100;
+
+	descuento=prompt("Ingrese el descuento","Ej:25");
 	descuento=parseInt(descuento);
 
-	importeConDescuento=importe-descuento;
-	document.getElementById('resultado').value=importeConDescuento;//usar sino recto precio+/- el precio* aumento(var)/100
-	
+	importeConDescuento=importe-importe*descuento/100;
+
+	document.getElementById('resultado').value=importeConDescuento;
+
 
 
 
