ac6047dcc204e04c81e58c7eabe33ef8b4acda75
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-05 10:33:53 -0300

    Ejercicio 1 hecho
    concatenacion y alert

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,14 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	var nombre;// variable lowupperuppercase
+	nombre="Roberta";// a variable se asigna contenido con igual
+	//alert("Nombre")//en comillas va contenido literal
+	alert(nombre);// para usar variable sin comillas
+	alert("Nombre de la alumna: "  + nombre);
+
+
+
+
 }
 
