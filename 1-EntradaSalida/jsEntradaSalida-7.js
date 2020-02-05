3e653f0ad6d156599f366f7d79ae455f3bcda09b
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-05 14:18:56 -0300

    Ejercicio 7 hecho
    suma, resta, multiplicacion, division

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,35 +3,73 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
-	var primerNumero;
-	var segundoNumero;
-	var resultadoDeSuma;//no van numeros en las variables
+{
+	var numeroUno;//no van numeros en las variable 1 es UNO
+	var numeroDos;
+	var resultadoDeSuma;
 
-	primerNumero=document.getElementById('numeroUno').value;
-	primerNumero=parseInt(primerNumero);//se parsea siempre en las ecuaciones numericas para definir que son núemros si o si
-	segundoNumero=document.getElementById('numeroDos').value;
-	segundoNumero=parseInt(segundoNumero);//parsear justo despues de asignar el valor
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);//se parsea en ecuaciones si o si para definir que es un caracter numerico
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);//parsear justo despues de asignar valor
 
-	resultadoDeSuma=primerNumero/segundoNumero;
-	console.log(resultadoDeSuma);//visualiza en cosola de inspeccion en chrome, es similar a alert, devuelve datos
+	resultadoDeSuma=numeroUno+numeroDos;
 
+	//alert(resultadoDeSuma);
+
+	console.log(resultadoDeSuma);//en vez de que lo vea el usuario lo ves en console de inspeccionar elemento
 
 	
 }
 
 function restar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultadoDeResta;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+
+	resultadoDeResta=numeroUno-numeroDos;
+
+	console.log(resultadoDeResta);
+
 	
 }
 
 function multiplicar()
 { 
+	var numeroUno;
+	var numeroDos;
+	var resultadoDeMultiplicacion;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+
+	resultadoDeMultiplicacion=numeroUno*numeroDos;
+
+	console.log(resultadoDeMultiplicacion);
 	
 }
 
 function dividir()
 {
-	
+	var numeroUno
+	var numeroDos
+	var resultadoDeDivision
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+
+	resultadoDeDivision=numeroUno/numeroDos;
+
+	console.log(resultadoDeDivision);
 }
 
