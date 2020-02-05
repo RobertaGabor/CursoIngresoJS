8d7acb93ce17ec54975ba83e7549b93f52b18988
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-05 20:31:50 -0300

    E/S Ej: 3 sabado
    consultar sábado si esta bien

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var precioConDescuento;
+
+	precio=prompt("Ingrese precio","Ej:200");
+	precio=parseInt(precio);
+
+	descuento=prompt("Ingrese descuento","Ej:25");
+	descuento=parseInt(descuento);
+
+	precioConDescuento=precio-precio*descuento/100;
+
+	document.getElementById('elPrecioFinal').value=precioConDescuento
+
 
 }
