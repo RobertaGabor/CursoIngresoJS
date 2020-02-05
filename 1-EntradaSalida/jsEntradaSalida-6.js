a7ec37a3aaa87b9bae62cb2da6213365f5c54f72
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-05 14:19:56 -0300

    Ejercicio 6 hecho
    suma

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,26 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultadoDeSuma;//el resultado es un valor aparte
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);//si saco el parseint los une como concatenaciones: 2+3=23
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+
+	resultadoDeSuma=numeroUno+numeroDos;//la suma de dos parseint da una suma real matematica
+
+	alert(resultadoDeSuma);
+
+
+
+	
+
+
+
+
 
 }
 
