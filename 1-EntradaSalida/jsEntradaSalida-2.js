93f133c2ff66e3d9321e2449108b0b854b8227e6
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-05 10:34:47 -0300

    Ejercicios 2 hecho
    prompts

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	var nombre; 
+	nombre = prompt("Ingrese su nombre");// al lado con coma se puede definir un nombre, prompt va con = 
+	alert("Su nombre es: " +nombre);// muestra lo ingresado en index, prompt devuelve datos asignados por el usuario ademas de mostrar
+
 	
 }
 
