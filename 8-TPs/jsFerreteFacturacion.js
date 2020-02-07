53203d3b44cd0a6ce195977bf7cb89f88eee0078
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-06 23:56:33 -0300

    Ferrete facturación hecho sábado
    ver sábado si esta bien

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,71 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var sumaDePrecios;
+
+	primerPrecio=document.getElementById('PrecioUno').value;
+	primerPrecio=parseInt(primerPrecio);
+	segundoPrecio=document.getElementById('PrecioDos').value;
+	segundoPrecio=parseInt(segundoPrecio);
+	tercerPrecio=document.getElementById('PrecioTres').value;
+	tercerPrecio=parseInt(tercerPrecio);
+
+	sumaDePrecios=primerPrecio+segundoPrecio+tercerPrecio;
+
+
+	console.log(sumaDePrecios);
+
 	
 }
 function Promedio () 
 {
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var sumaDePrecios;//se puede hacer directo la suma entre parentesis dividido 3?
+	var promedioDePrecios;
+
+	primerPrecio=document.getElementById('PrecioUno').value;
+	primerPrecio=parseInt(primerPrecio);
+	segundoPrecio=document.getElementById('PrecioDos').value;
+	segundoPrecio=parseInt(segundoPrecio);
+	tercerPrecio=document.getElementById('PrecioTres').value;
+	tercerPrecio=parseInt(tercerPrecio);
+
+	sumaDePrecios=primerPrecio+segundoPrecio+tercerPrecio;
+
+	promedioDePrecios=sumaDePrecios/3;
+	promedioDePrecios=parseInt(promedioDePrecios);
+
+	console.log(promedioDePrecios);
 	
 }
 function PrecioFinal () 
 {
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var sumaDePrecios;
+	var precioFinal;
+
+	primerPrecio=document.getElementById('PrecioUno').value;
+	primerPrecio=parseInt(primerPrecio);
+	segundoPrecio=document.getElementById('PrecioDos').value;
+	segundoPrecio=parseInt(segundoPrecio);
+	tercerPrecio=document.getElementById('PrecioTres').value;
+	tercerPrecio=parseInt(tercerPrecio);
+
+	sumaDePrecios=primerPrecio+segundoPrecio+tercerPrecio;
+
+	precioFinal=sumaDePrecios+sumaDePrecios*21/100;
+	precioFinal=parseInt(precioFinal);
+
+
+	console.log(precioFinal);
+
+
 	
 }
