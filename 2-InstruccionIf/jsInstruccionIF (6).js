016868304e8d53bb0fc5a60cd37dc6b68454e887
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-11 12:17:52 -0300

    IF 6
    3 if 2 else

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,24 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
 
 
+	if(edad>17)//proceso
+	{
+		alert("Mayor");
+	}
 
-
-}//FIN DE LA FUNCIÓN
+	else//no es un proceso
+	{
+		if(edad<13)//proceso, en vez de 3 if y 4 procesos, van con else y se ahorra
+		{
+			alert("Menor");
+		}
+		else
+		{
+			alert("Adolescente");
+		}
+	}	
+}//que los puntos toquen la llave
