ca72bd1796a19ffc6f97e83151353b7986f24ebb
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-11 12:14:08 -0300

    IF 1
    if con igualdad

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,9 @@
 function mostrar()
 {
-//tomo la edad  
-
-
-
+	var edad;
+	edad=document.getElementById('edad').value;
+	if (edad==15)//no lleva ; y el signo = se repite, no va solo (sino estarias asignando)
+	{
+		alert("niña bonita");
+	}				
 }//FIN DE LA FUNCIÓN
