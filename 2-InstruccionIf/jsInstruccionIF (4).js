bd7ffb7d4564e7f3612865bd43932784333357d8
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-11 12:16:21 -0300

    IF 4
    if &&

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+
+	if(edad<18)//if(edad<18&&edad>12)
+	{
+		if(edad>12)
+		{
+			alert("adolescente");
+		}
+
+	}
+
 
 
 
