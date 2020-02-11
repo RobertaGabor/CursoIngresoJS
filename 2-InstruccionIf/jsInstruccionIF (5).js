3bb02bcf1e07dd905a5bd62c86b92072f9a211a1
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-11 12:17:08 -0300

    IF 5
    if negado

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
 
-
+	if(!(edad<17&&edad>12))//if(edad>17||edad<12)
+	{
+		alert("No adolescente");
+  	}
+  	
 }//FIN DE LA FUNCIÓN
