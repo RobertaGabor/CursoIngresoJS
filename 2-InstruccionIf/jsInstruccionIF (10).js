00a8d1abbe643dfe26d89f55bc0fad3292ff6243
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-11 22:29:41 -0300

    IF 10
    random asignacion

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,25 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+	var random;
 
+	random=Math.floor(Math.random() * 10) + 1;//random es de 0 a 1 (0,999) multiplicado por 10 o 100 etc es de 0 a 10(9,999) y ese 1 agarra al 10 y al 0 conevrtido en 1 (1 a 10)
+	random=parseInt(random);
+
+	if(random<4)
+	{
+		alert("Vamos, la proxima se puede");
+	}
+	else
+	{
+		if(random==9||random==10)
+		{
+			alert("EXELENTE");
+		}
+		else
+		{
+			alert("APROBÓ");
+		}
+	}
+	
+	console.log(random);
 }//FIN DE LA FUNCIÓN
