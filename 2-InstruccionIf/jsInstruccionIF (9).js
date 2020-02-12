ffa0f3bdc9212c6aa06b50290ef6dc54d69d17dc
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-11 22:29:02 -0300

    IF 9
    random

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,9 @@
-function mostrar()
+function mostrar() 
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+	var random;
 
+	random=Math.floor(Math.random() * 10) + 1;//random es de 0 a 1 (0,999) multiplicado por 10 o 100 etc es de 0 a 10(9,999) y ese 1 agarra al 10 y al 0 conevrtido en 1 (1 a 10)
+	random=parseInt(random);
+	
+	console.log(random);
 }//FIN DE LA FUNCIÓN
