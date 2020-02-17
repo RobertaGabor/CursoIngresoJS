86463b41906a79c5fc03cee095a9d8501b2e0b7a
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-16 23:23:00 -0300

    IF
    con concatenado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,35 @@
 function mostrar()
 {
+	var primerNumero
+	var segundoNumero
+	var suma
+
+	primerNumero=prompt("Ingrese numero","ej: 2");
+	segundoNumero=prompt("Ingrese numero","ej: 3");
+
+
+	if(primerNumero==segundoNumero)
+	{
+		alert(primerNumero+segundoNumero);
+	}
+	else
+	{
+		if(primerNumero>segundoNumero)
+		{
+			alert(primerNumero-segundoNumero);
+		}
+		else
+		{
+			primerNumero=parseInt(primerNumero);
+			segundoNumero=parseInt(segundoNumero);
+			suma=primerNumero+segundoNumero;
+			alert(suma);
+		}
+	}
+
+	if(suma>10)
+	{
+		alert("La suma es " + suma + " y supero el 10");
+	}
 
 }
