4b6536549c1541ffed77a6561d9ed770a0073f0e
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-17 18:58:15 -0300

    Adivina el numero 1 hecho

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -7,20 +7,42 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
+var numeroIngresado; 
 var contadorIntentos;
+var numeroSecreto;
 
-function comenzar()
+function comenzar()//va solo lo que se ejecuta con este boton
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+	numeroSecreto=Math.floor(Math.random() * 100) + 1;//va antes de poner nuestro numero
+	numeroSecreto=parseInt(numeroSecreto);
+	console.log(numeroSecreto);
 
+
+	contadorIntentos=0;//se define antes de la primera vuelta
 }
 
-function verificar()
+function verificar()//va lo que se ejecuta con este boton, que esta en consideracion
 {
-	
-	
+	numeroIngresado=document.getElementById('numero').value;
+	numeroIngresado=parseInt(numeroIngresado);
+
+	if(numeroIngresado==numeroSecreto)
+	{
+		alert("Usted es un ganador!!! y en solo " +contadorIntentos + " intentos");
+	}
+	else
+	{
+		if(numeroIngresado>numeroSecreto)
+		{
+			alert("Se pasó");
+		}
+		else
+		{
+			alert("Le falta");
+		}
+	}
+
+	contadorIntentos=contadorIntentos+1;//se va anotando cada vez que verificas
+
+	document.getElementById('intentos').value=contadorIntentos;	
 }
