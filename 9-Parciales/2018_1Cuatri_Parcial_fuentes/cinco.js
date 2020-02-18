fef253fe43fb41ca59ba4d665e19c6bf3347cf06
alumno <alumno>
2020-02-18 11:43:43 -0300

    switch parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+	var planeta
+
+	planeta=prompt("ingrese planeta","Ej:mercurio");
+
+	switch(planeta)
+	{
+		case "tierra":
+		console.info("Acá vivimos");
+		break;
+		case "mercurio":
+		case "venus":
+		console.info("Acá hace más calor");
+		break;
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "urano":
+		case "neptuno":
+		case "pluton":
+		console.info("acá hace más frio");
+		break;
+		default:
+		console.info("No es un planeta");
+	}
 
 }
