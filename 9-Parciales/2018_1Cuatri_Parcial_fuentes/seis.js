a80c5a9d2493cdaa44afdb56de2a584e325698c6
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-18 21:30:30 -0300

    EJ if+switch hecho
    ponele

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,51 @@
 function mostrar()
 {
+	var hora
+	hora=document.getElementById('laHora').value;
+	hora=parseInt(hora);
 
+	switch(hora)
+	{
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		console.info("Es de mañana");
+		break;
+
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+		case 19:
+		console.info("Es de tarde");
+		break;
+
+		case 20:
+		case 21:
+		case 22:
+		case 23:
+		case 24:
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		console.info("Es de noche");
+		break;
+
+		default:
+		console.info("Hora no valida");
+	}
+
+	if(hora>19&&hora<24)
+	{
+		console.info("A dormir");
+	}
 }
