87137ae73bc0458aef6878c338133ca8d75eeefc
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-18 22:06:23 -0300

    while EJ1
    ponele

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,13 @@
 function mostrar()
 {
-	alert('iteración while');
+	var iteracion;
+	iteracion=0;
+	iteracion=parseInt(iteracion);
 
 
+	while (iteracion<10)//10 incluido
+	{
+		console.log(1+iteracion);// numero es de donde empieza
+		iteracion=iteracion+1;
+	}
 }//FIN DE LA FUNCIÓN
