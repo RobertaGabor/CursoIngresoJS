1fa9f241609fcd349203a804b56def9d94f53a39
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-26 00:05:00 -0300

    tp adivina numero 2 hecho
    en casa corregir

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -11,18 +11,52 @@ Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
 var contadorIntentos;
+var numero;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-	
-
+	numeroSecreto=Math.floor(Math.random() * 100)+1;
+	numeroSecreto=parseInt(numeroSecreto);
+	console.log(numeroSecreto);
+	contadorIntentos=0;//definir antes de verificar
 }
 
 function verificar()
-{
-	
-	
+{	
+	numero=document.getElementById('numero').value;
+	contadorIntentos=contadorIntentos+1;
 
+	if(numeroSecreto==numero)
+	{
+		switch(contadorIntentos)
+		{
+			case 1:
+			alert("usted es un Psíquico");
+			break;
+			case 2:
+			alert("excelente percepción");
+			break;
+			case 3:
+			alert("Esto es suerte");
+			break;
+			case 4:
+			alert("Excelente técnica");
+			break;
+			case 5:
+			alert("usted está en la media");
+			break;
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			alert("Falta técnica");
+			break;
+			default:
+			alert("afortunado en el amor");
+		}
+	}
+	else
+	{
+		alert("Incorrecto :(")
+	}
 }
