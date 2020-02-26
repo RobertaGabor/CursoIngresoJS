8f96c8c05190cbe1edd94fac1580b09df9ac9de2
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-26 18:16:58 -0300

    ej 6 hecho
    paresss

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,22 @@
 function mostrar()
-{
+{	
+	var numeroIngresado;
+	var i;
+	var contadorPares;
 
+	contadorPares=0;
+	numeroIngresado=prompt("Ingrese numero");
+	numeroIngresado=parseInt(numeroIngresado);
 
-
-
+	for(i=2/*minimo divisor par*/;i<numeroIngresado;i=i+2)//i empieza de 2 porque si empieza de 1 y se agregan 2, cambia par e impar
+	//i=1 con el i+1 cambia valor par, 1 y +2 es para impar porque 1 es el primer divisor impar. 
+	{
+		//if(i%2==0) ya se valida con el +2, si pongo 1 si necesito
+		//{
+			console.info("Par: "+i);
+			contadorPares++;
+		//}
+	}
+	
+	console.info("Cantidad de pares: "+contadorPares);
 }//FIN DE LA FUNCIÓN
