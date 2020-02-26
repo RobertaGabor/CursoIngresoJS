d48c53d05bc47474640b25b3b9ec40ef7615d9de
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-26 00:02:24 -0300

    EJ 8 hecho
    en casa ponele

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,31 +1,40 @@
 function mostrar()
 {
 
+	var numero
 	var contador;
 	var positivo;
 	var negativo;
 	var respuesta;
-	var numero;
 
 	contador=0;
 	positivo=0;
 	negativo=1;
-	respuesta="Si";
+	respuesta="si";
 
-	while(respuesta=="Si")
+	while(respuesta=="si")
 	{
-		numero=prompt("Ingrese numero");
+		numero=prompt("Ingrese un número");
 		numero=parseInt(numero);
 
-		contador=contador+1;
-		positivo=positivo+numero;
-		negativo=negativo-numero;
+		contador=contador+1
 
-		respuesta=prompt("escribir Si para continuar, sino finalizará");
+		if(numero>0)
+		{
+			console.log(numero);
+			positivo=positivo+numero;	
+		}
+		else
+		{
+			if(numero<0)//si lo hago al reves puedo sacar el segundo if y dejarlo en ese
+			{		
+				console.log(numero);
+				negativo=negativo*numero;
+			}
+		}	
+		respuesta=prompt("Ingrese si para continuar, sino finalizará la operacion","Ej:si");
 	}
 
-
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
-
+	document.getElementById('suma').value=positivo;
+	document.getElementById('producto').value=negativo;
 }//FIN DE LA FUNCIÓN
