08b82cc4930ed45fa14ede4965eb3077a70c18f7
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-26 00:08:16 -0300

    ej 7 sin terminar
    no podia mas

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,5 +1,35 @@
 function mostrar()
 {
-	
-}
+	var nota;
+	var sexo;
+	var promedio;
+	var contador;
+	var sumaTotal;
 
+	contador=0;
+	contador=parseInt(contador);//preguntar xq parseo
+	sumaTotal=0;
+
+	while(contador<5)
+	{ 
+		nota=prompt("Ingrese nota");
+		nota=parseInt(nota);
+		sexo=prompt("Ingrese sexo");
+		contador=contador+1;
+		sumaTotal=sumaTotal+nota;
+
+		while(isNaN(nota)||nota<0||nota>10)
+		{
+			nota=prompt("Ingrese nota");
+		}
+		while(sexo!="m"&&sexo!="f")
+		{
+			sexo=prompt("Ingrese sexo");
+		}
+    }
+
+
+    promedio=sumaTotal/contador;
+    alert("promedio de notas totales: "+promedio);
+
+}
