ea7d5b83ae276daf630c3d13ab768aded7a5283f
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-26 00:05:49 -0300

    raising btl no funciona
    preguntar profe q onda

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,95 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+ 	var edad;
+ 	var sexo;
+ 	var estadoCivil;
+ 	var sueldoBruto;
+ 	var numeroLegajo;
+ 	var nacionalidad;
+
+ 	sueldoBruto=0;
+ 	estadoCivil=0;
+
+ 	while(isNaN(edad)||edad<18||edad>90)
+ 	{
+ 		edad=prompt("ingresar edad");
+ 	}
+
+ 	while(sexo!="m"&&sexo!="f")
+ 	{
+ 		sexo=prompt("Ingrese sexo","ej: m o f");
+ 	}
+
+ 	while(/*isNaN(estadoCivil)||*/estadoCivil<1||estadoCivil>4)//lo contrario para que pida devuelta lo que esta mal
+ 	{
+ 		estadoCivil=prompt("Ingrese 1-para soltero, 2-para casado, 3-para divorciado, 4-para viudo");
+
+ 		if(estadoCivil==4)
+ 		{
+ 			estadoCivil="Viudo";
+ 		}
+ 		else
+ 		{
+ 			if(estadoCivil==3)
+ 			{
+ 				estadoCivil="Divorciado";
+ 			}
+ 			else
+ 			{
+ 				if(estadoCivil==2)
+ 				{
+ 					estadoCivil="Casado";
+ 				}
+ 				else
+ 				{
+ 					if(estadoCivil==1)
+ 					{
+ 					 	estadoCivil="Soltero";
+ 					}
+ 				}
+ 			}
+ 		}
+ 	}
+ 	
+ 	while(isNaN(sueldoBruto) || sueldoBruto<8000)
+ 	{
+ 		sueldoBruto=prompt("Ingrese sueldo bruto");
+ 	}
+
+ 	while(isNaN(numeroLegajo)|| numeroLegajo<1000||numeroLegajo>9999)
+ 	{
+ 		numeroLegajo=prompt("Ingrese numero de legajo");
+ 	}
+
+ 	while(nacionalidad!="A"&&nacionalidad!="E"&&nacionalidad!="N")
+ 	{
+ 		nacionalidad=prompt("Ingrese “A” para argentinos, “E” para extranjeros, “N” para nacionalizados","ej: A");
+
+ 		if(nacionalidad=="A")//xq se rompe el if, bucle
+ 		{
+ 			nacionalidad="Argentino";
+ 		}
+ 		else
+ 		{
+ 			if(nacionalidad=="E")
+ 			{
+ 				nacionalidad="Extranjero";
+ 			}
+ 			else
+ 			{
+ 				if(nacionalidad=="N")
+ 				{
+ 					nacionalidad="Nacionalizado";
+ 				}
+ 			}
+ 		}
+ 	}
+ 	
+ 	document.getElementById('Edad').value=edad;
+ 	document.getElementById('Sexo').value=sexo;
+ 	document.getElementById('EstadoCivil').value=estadoCivil;
+ 	document.getElementById('Sueldo').value=sueldoBruto;
+ 	document.getElementById('Legajo').value=numeroLegajo;
+ 	document.getElementById('Nacionalidad').value=nacionalidad;
 }
