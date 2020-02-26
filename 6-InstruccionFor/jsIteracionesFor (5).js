60727eecc3428687a965d52a11eac1852df741fd
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-26 18:16:31 -0300

    ej 5 hecho

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,7 +1,18 @@
 function mostrar()
 {
 
+ 	var i;
+ 	var numero;
+ 	numero=prompt("ingrese numero");
+ 	numero=parseInt(numero);
 
-
-
+ 	for(i=0;numero!=9;)
+ 	{
+ 		if (numero==9)
+ 		{
+ 			break;
+ 		}
+ 		numero=prompt("ingrese numero");
+ 		numero=parseInt(numero);
+ 	}
 }//FIN DE LA FUNCIÓN
