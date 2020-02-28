82907e5e64786ff7724f4c06f2d5c69890c61e63
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-02-28 15:16:44 -0300

    ej 7 hecho
    si

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,21 @@
 function mostrar()
 {
 
+	var numero;
+	var i;
+	var cantidadDivisores;
 
+	numero=prompt("Ingrese numero");
+	numero=parseInt(numero);
+	cantidadDivisores=0;
 
-
+	for(i=0;i<numero+1;i++)
+	{
+		if(numero%i==0)
+		{
+			console.info("Divisor: "+i);
+			cantidadDivisores++;
+	    }
+	}
+	console.info("Cantidad de divisores: "+ cantidadDivisores);
 }//FIN DE LA FUNCIÓN
