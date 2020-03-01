5cde1eff9a0b1820c8cd1a5e52dee43bacad5288
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-03-01 19:33:48 -0300

    juego hecho
    no t olvides de la existencia de switch

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -4,26 +4,54 @@ siendo 1 para “piedra”, el 2 para “papel” y 3 para “tijera”.
 El jugador seleccionará una imagen correspondiente 
 a su opción  y le informaremos si ganó, empató o perdió.
 */
-var eleccionMaquina;
-
+var random;//definir variable afuera para que este en tods, la asignacion se sostiene
 function comenzar()
 {
-	
-
-
-}//FIN DE LA FUNCIÓN
+	random=Math.floor(Math.random() *3) +1;
+	console.info("Numero random: "+random);
+}
 function piedra()
 {
-	
-
+	switch(random)
+	{
+		case 1:
+		alert("Empataste");
+		break;
+		case 2:
+		alert("Perdiste");
+		break;
+		case 3:
+		alert("Ganaste");
+		break;
+	}
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-
-
+	switch(random)
+	{
+		case 1:
+		alert("Ganaste");
+		break;
+		case 2:
+		alert("Empataste");
+		break;
+		case 3:
+		alert("Perdiste");
+		break;
+	}
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
-
+	switch(random)
+	{
+		case 1:
+		alert("Perdiste");
+		break;
+		case 2:
+		alert("Ganaste");
+		break;
+		case 3:
+		alert("Empataste");
+		break;
+	}
 }//FIN DE LA FUNCIÓN
