c747c2253f2c3564158c9abf5af361ce27f985be
RobertaGabor <60522982+RobertaGabor@users.noreply.github.com>
2020-03-01 20:52:39 -0300

    sin terminar no entiendo

--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -1,31 +1,78 @@
 var eleccionMaquina;
-var ContadorDeEmpates=0;
-var ContadorDeGanadas=0;
-var ContadorDePerdidas=0;
+var contadorDeEmpates;
+var contadorDeGanadas;
+var contadorDePerdidas;
+
+
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 3
-	 	
-
-
-
+	eleccionMaquina=Math.floor(Math.random()*3 )+1;
+	console.log(eleccionMaquina);
+	contadorDeEmpates=0;
+	contadorDePerdidas=0;
+	contadorDeGanadas=0;
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+	switch(eleccionMaquina)
+	{
+		case 1:
+		alert("Empataste");
+		contadorDeEmpates++;
+		break;
+		case 2:
+		alert("Perdiste");
+		contadorDePerdidas++;
+		break;
+		case 3:
+		alert("Ganaste");
+		contadorDeGanadas++;
+		break;
+	}
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-
+	switch(eleccionMaquina)
+	{
+		case 1:
+		alert("Ganaste");
+		contadorDeGanadas++;		
+		break;
+		case 2:
+		alert("Empataste");
+		contadorDeEmpates++;
+		break;
+		case 3:
+		alert("Perdiste");
+		contadorDePerdidas++;		
+		break;
+	}
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	switch(eleccionMaquina)
+	{
+		case 1:
+		alert("Perdiste");
+		contadorDePerdidas++;		
+		break;
+		case 2:
+		alert("Ganaste");
+		contadorDeGanadas++;		
+		break;
+		case 3:
+		alert("Empataste");
+		contadorDeEmpates++;
+		break;
+	}
+
 }//FIN DE LA FUNCIÓN
 
-function mostrarResultado()
+function mostrarResultado(contadorDeGanadas,contadorDePerdidas,contadorDeEmpates)
 {
-
+	document.getElementById('ganadas').value=contadorDeGanadas;
+	document.getElementById('perdidas').value=contadorDePerdidas;
+	document.getElementById('empatadas').value=contadorDeEmpates;
 }
